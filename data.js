eba33d4347593a6247ee81d66e04847b5b3d22c4
Your Name <tomasdepiano1@gmail.com>
2026-03-04 09:58:59 -0700

    Added more css to this project and added marias wording and one photo of hers

---
+++ data.js
@@ -0,0 +1,41 @@
+const birthdayMessages = [
+  {
+    id: 1,
+    name: "Maria",
+    message: `
+      Happy Birthday Bruder 🤍
+
+You have always been my safe place, my biggest supporter, and one of the most loving and caring people I know. I’m so lucky God chose you to be my older sister.
+
+Thank you for protecting me, guiding me, and loving me the way you do. I truly don’t know what I’d do without you. I hope this year brings you so much happiness I love you big bruder 🤍🧁
+    `,
+    photos: ["./images/IMG_8270.jpeg"],
+  },
+
+  {
+    id: 2,
+    name: "Sarah",
+    message: `
+      Happy Birthday to one of the most incredible
+      women I know!
+
+      Your laugh is contagious, your heart is golden,
+      and your friendship means everything to me.
+      I’m so lucky to have you in my life.
+    `,
+    photos: ["images/sarah1.jpg"],
+  },
+
+  {
+    id: 3,
+    name: "Mike",
+    message: `
+      Happy Birthday!!
+
+      I’ll never forget our adventures and the way
+      you always bring energy and love into every
+      moment. Keep shining.
+    `,
+    photos: [],
+  },
+];
--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-const card = document.getElementById("card");
+const startBtn = document.getElementById("startBtn");
+const intro = document.getElementById("intro");
+const gallery = document.getElementById("gallery");
+const container = document.getElementById("cardContainer");
 
-card.addEventListener("click", () => {
-  card.classList.toggle("open");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("closeBtn");
+const modalName = document.getElementById("modalName");
+const modalMessage = document.getElementById("modalMessage");
+const modalPhotos = document.getElementById("modalPhotos");
+
+startBtn.addEventListener("click", () => {
+  intro.classList.add("hidden");
+  gallery.classList.remove("hidden");
+});
+
+birthdayMessages.forEach((person) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  card.innerText = person.name;
+
+  card.addEventListener("click", () => openLetter(person));
+  container.appendChild(card);
+});
+
+function openLetter(person) {
+  modalName.innerText = person.name;
+  modalMessage.innerText = person.message;
+
+  modalPhotos.innerHTML = "";
+
+  person.photos.forEach((photo) => {
+    const img = document.createElement("img");
+    img.src = photo;
+    modalPhotos.appendChild(img);
+  });
+
+  modal.style.display = "flex";
+}
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
 });
